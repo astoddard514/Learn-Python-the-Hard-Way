dc1ccbfb326ea88b293b631c26d8f73a6505e830
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-01 22:21:17 -0600

    Add exercise 12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#we can rewrite exercise 11 by using y = input("Name? ") for the variables
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
