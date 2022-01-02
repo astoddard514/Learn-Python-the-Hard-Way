beed8a5b3f8f9c659f5a126c23ac7da1f90cb9c2
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-02 14:33:41 -0600

    Add more exercise 16 files

--- ex16.3.py
+++ ex16.3.py
@@ -22,12 +22,11 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"""
+    {line1}  
+    {line2}
+    {line3}
+""") # escape \n is redundant in this format
 
 print("And finally, we close it.")
 target.close()
