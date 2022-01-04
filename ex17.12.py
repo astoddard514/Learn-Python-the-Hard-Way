c013361c88e9ec3e4de51d979b10a3bd6c3814cf
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-04 16:50:29 -0600

    Add exercise 17 files

---
+++ ex17.12.py
@@ -0,0 +1,22 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+# we could do these two on one line, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
+
+# echo "This is a test" > test.txt
+# cat test.txt
+# python ex17.py test.txt new_file.txt
