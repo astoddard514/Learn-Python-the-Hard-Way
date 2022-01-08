a46f1b48be16f37fa332fdb38c50da6d51419b04
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-08 15:13:45 -0600

    added ex18 note
    explanation note from text line 3

--- ex18.py
+++ ex18.py
@@ -1,6 +1,6 @@
 # this one is like your scripts with argv
 def print_two(*args):
-    arg1, arg2 = args
+    arg1, arg2 = args #unpacks the arguments
     print(f"arg1: {arg1}, arg2: {arg2}")
 
 # ok, that *args is actually pointless, we can just do this
