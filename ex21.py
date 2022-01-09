3335439a252d7f7d7c37881ee1ad04c03d5bd048
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-08 23:19:03 -0600

    Add ex21 puzzle note

--- ex21.py
+++ ex21.py
@@ -26,6 +26,5 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 # A puzzle for extra credit, type it in anyway.
 print("Here is a puzzle.")
 
-what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
-
+what = add(age, subtract(height, multiply(weight, divide(iq, 2)))) # starts in the parentheses and works it way out. 
 print("That becomes: ", what, "Can you do it by hand?")
