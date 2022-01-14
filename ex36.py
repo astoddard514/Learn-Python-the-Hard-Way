5bf4ca97985e768c2f37b1203fbb14a5941c6554
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-14 12:59:04 -0600

    choose your own adventure outline

---
+++ ex36.py
@@ -0,0 +1,21 @@
+from sys import exit
+
+def cave_spooky():
+    print("Luna and Gus slowly enter Cave Spooky and ")
+    #choices: sneak by (find wand) or scream (die)
+
+def deep_woods():
+    print("Ahead in the trees, Luna sees the eyes of a tiger. She taps Gus to show him.")
+    #choices: climb a tree (escape) or run (die)
+
+def dinosaur_land():
+    print("Luna and Gus leave their neighborhood and start across dinosaur-land. Suddenly, overhead, Gus spots a pterodatyl flying.")
+    # choices: hide (escape) or throw rocks (die)
+
+def dead():
+    print("You died.")
+
+def start():
+    print("\nLuna and Gus need to find the magical wand to save Mom and Dad from evil robots!\n\n They will need to pass across dinosaur-land, through the deep woods, and into Cave Spooky to get to the wand. It will a very difficult journey.\n")
+
+start()
