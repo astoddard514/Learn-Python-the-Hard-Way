37cec09d5d3827c77f396835be8752b4627a2f82
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-15 23:29:12 -0600

    add triple quotes formatting

--- ex36.py
+++ ex36.py
@@ -5,27 +5,38 @@ def cave_spooky():
     # choices: sneak by (find wand) or scream (die)
 
 def deep_woods():
-    print("Ahead in the trees, Luna sees the eyes of a tiger. She taps Gus to show him. They need to get away!\n\n=============================================\n")
+    print("""
+    Ahead in the trees, Luna sees the eyes of a tiger. She taps Gus to show him. 
+    They need to get away!
+    \n\n=============================================\n
+    """)
     # choices: climb a tree (escape) or run (die)
     # use while True loop for Tiger leaving
 
 def dinosaur_land():
-    print("Suddenly, Gus spots a pterodatyl flying overhead! It's looking for its next meal.\n\n**********************************************\n")
+    print(
+        """\tSuddenly, Gus spots a pterodactyl flying overhead! 
+        It's looking for its next meal.  
+        \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
+        """)
     print("SHOULD LUNA AND GUS 'hide' UNDER A BUSH OR 'throw rocks' AT THE PTERODACTYL?")
     print("TYPE 'hide' OR 'throw rocks'\n")
     
     choice = input("> ")
 
     if choice == "hide":
-        print("\n=============================================\n\nThe pterodactyl doesn't see them, and flies further into the valley. Luna and Gus continue on and arrive at the deep woods.")
+        print("""
+        \n===========================================\n\n
+        The pterodactyl doesn't see them, and flies further into the valley. Luna and Gus continue on and arrive at the deep woods.
+        """)
         deep_woods()
     elif choice == "throw rocks":
-        dead("\n=============================================\n\nThe pterodactyl swoops down and captures you both in its claws. It brings you back to its family for dinner.\n\n=============================================\n")
+        fail("\n=============================================\n\nThe pterodactyl swoops down and captures you both in its claws. It brings you back to its family for dinner.\n\n=============================================\n")
     else:
-        dead("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
+        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
 
-def dead(why):
-    print(why, "AND YOU DIED.\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
+def fail(why):
+    print(why, "AND YOU FAILED TO RETRIEVE THE WAND.\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
     print("TRY AGAIN? 'yes' and 'no'")
 
     choice = input("> ")
@@ -35,21 +46,42 @@ def dead(why):
     elif choice == "no":
         print("BYE!")
     else:
-        dead("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYou just typed in robot language and the robots found you with their lasers.\n")
+        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYou just typed in robot language and the robots found you with their lasers.\n")
 
 def start():
-    print("\n=============================================\n\nSTART: \nLuna and Gus have to find the magical wand to save Mom and Dad from EVIL ROBOTS! They will need to pass across dinosaur-land, through the deep woods, and into Cave Spooky to get to the magic wand. It will a be very difficult journey.\n\n=============================================\n")
+    print(
+        """
+        \n==============================================================\n
+        START:
+        Luna and Gus have to find the magical 
+        wand to save Mom and Dad from EVIL ROBOTS! 
+        They will need to pass across dinosaur-land, 
+        through the deep woods, and into Cave Spooky 
+        to get to the magic wand. It will a be very 
+        difficult journey.\n\n==============================================================\n
+        """)
     print("ARE YOU READY TO HELP MOM AND DAD?")
     print("TYPE 'yes' or 'no'\n")
 
     choice = input("> ")
 
     if choice == "yes":
-        print("\n**********************************************\n\nBrave Luna and strong Gus leave their neighborhood and start across dinosaur-land.")
+        print(
+        """
+        \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
+        Brave Luna and strong Gus leave their neighborhood 
+        and start across dinosaur-land.
+            """)
         dinosaur_land()
     elif choice == "no":
-        dead("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTHE ROBOTS HAVE STOLEN YOUR MOM AND DAD FOREVER. SO SAD. YOU STARVE WITH NO FOOD.")
+        fail(
+        """
+        \nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+        THE ROBOTS HAVE STOLEN YOUR MOM AND DAD FOREVER. 
+        SO SAD. YOU GIVE UP ON FINDING THE WAND AND 
+        SURVIVE ON BERRIES AND SEEDS.
+        """)
     else:
-        dead("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
+        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
 
 start()
