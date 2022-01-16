a028890622a3ecb7b8dac93e0bca9fef1883a662
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-16 01:22:25 -0600

    set up boar choice

--- ex36.py
+++ ex36.py
@@ -1,22 +1,37 @@
 from sys import exit
 
 def cave_spooky():
-    print("\nLuna and Gus slowly enter Cave Spooky and their eyes adjust to the darkness. At the back of the cave the notice a huge troll sleeping, snoring loudly.\n")
+    print("""
+    Luna and Gus slowly enter Cave Spooky and their eyes adjust to the darkness. At the back of the cave the notice a huge troll sleeping, snoring loudly.\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n""")
     # choices: sneak by (find wand) or scream (die)
 
 def deep_woods():
-    print("""
-    Ahead in the trees, Luna sees the eyes of a tiger. She taps Gus to show him. 
-    They need to get away!
-    \n\n=============================================\n
+    print("""    Ahead in the trees, Luna sees the glimmering eyes 
+    of a wild boar. She taps Gus to show him. They need to get away!
+    \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
     """)
-    # choices: climb a tree (escape) or run (die)
-    # use while True loop for Tiger leaving
+    print("SHOULD LUNA AND GUS climb a tree or run away?")
+    print("TYPE 'climb' or 'run'")
+    boar_left = False
+
+    while True:
+        choice = input("> ")
+
+        # kids have to throw snack, THEN try to run. Want to use a while loop like in the execise, using a boolean
+        if choice == "run":
+            fail("The boar chases you out of the deep woods, away from Cave Spooky.")
+        elif choice == "climb": 
+            print()
+
+            cave_spooky()
+        else:
+            fail("\n*************************************************************\n\n    You just typed in robot language and they found you with their lasers.")
+    
 
 def dinosaur_land():
-    print(
-        """\tSuddenly, Gus spots a pterodactyl flying overhead! 
-        It's looking for its next meal.  
+    print("""
+    Suddenly, Gus spots a pterodactyl flying overhead! 
+    It's looking for its next meal.  
         \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
         """)
     print("SHOULD LUNA AND GUS 'hide' UNDER A BUSH OR 'throw rocks' AT THE PTERODACTYL?")
@@ -26,17 +41,22 @@ def dinosaur_land():
 
     if choice == "hide":
         print("""
-        \n===========================================\n\n
-        The pterodactyl doesn't see them, and flies further into the valley. Luna and Gus continue on and arrive at the deep woods.
+    \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
+    The pterodactyl doesn't see them, and flies 
+    further into the valley. Luna and Gus continue 
+    on and arrive at the deep woods.
         """)
         deep_woods()
     elif choice == "throw rocks":
-        fail("\n=============================================\n\nThe pterodactyl swoops down and captures you both in its claws. It brings you back to its family for dinner.\n\n=============================================\n")
+        fail("""
+    \n*************************************************************\n
+    The pterodactyl swoops down and captures you 
+    both in its claws. It brings you back to its family for dinner.""")
     else:
-        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
+        fail("\n*************************************************************\n\n    You just typed in robot language and they found you with their lasers.")
 
 def fail(why):
-    print(why, "AND YOU FAILED TO RETRIEVE THE WAND.\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
+    print(why, "\n    YOU FAIL TO RETRIEVE THE WAND.\n\n*************************************************************\n")
     print("TRY AGAIN? 'yes' and 'no'")
 
     choice = input("> ")
@@ -46,19 +66,20 @@ def fail(why):
     elif choice == "no":
         print("BYE!")
     else:
-        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYou just typed in robot language and the robots found you with their lasers.\n")
+        fail("\n*************************************************************\n\n    You just typed in robot language and they found you with their lasers.")
 
 def start():
     print(
         """
         \n==============================================================\n
-        START:
-        Luna and Gus have to find the magical 
-        wand to save Mom and Dad from EVIL ROBOTS! 
-        They will need to pass across dinosaur-land, 
-        through the deep woods, and into Cave Spooky 
-        to get to the magic wand. It will a be very 
-        difficult journey.\n\n==============================================================\n
+    START:
+    Luna and Gus have to find the magical 
+    wand to save Mom and Dad from EVIL ROBOTS! 
+    They will need to pass across dinosaur-land, 
+    through the deep woods, and into Cave Spooky 
+    to get to the magic wand. It will a be very 
+    difficult journey.
+        \n==============================================================\n
         """)
     print("ARE YOU READY TO HELP MOM AND DAD?")
     print("TYPE 'yes' or 'no'\n")
@@ -68,20 +89,18 @@ def start():
     if choice == "yes":
         print(
         """
-        \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
-        Brave Luna and strong Gus leave their neighborhood 
-        and start across dinosaur-land.
-            """)
+    \n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n
+    Brave Luna and strong Gus leave their neighborhood 
+    and start across dinosaur-land.""")
         dinosaur_land()
     elif choice == "no":
         fail(
         """
-        \nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
-        THE ROBOTS HAVE STOLEN YOUR MOM AND DAD FOREVER. 
-        SO SAD. YOU GIVE UP ON FINDING THE WAND AND 
-        SURVIVE ON BERRIES AND SEEDS.
-        """)
+        \n*************************************************************\n
+    The robots have stolen your mom and dad forever. 
+    So sad. You give up on finding the wand and 
+    survive like wildlings on berries and seeds.""")
     else:
-        fail("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nYOU JUST TYPED IN ROBOT LANGUAGE AND THEY FOUND YOU WITH THEIR LASERS.\n")
+        fail("\n*************************************************************\n\n    You just typed in robot language and they found you with their lasers.")
 
 start()
