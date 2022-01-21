0294409266397b5b985b38d59a6cfcc3bf8888ba
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-20 18:57:37 -0600

    Add exercise 40

--- ex40.py
+++ ex40.py
@@ -1,12 +1,16 @@
+
 class Song(object):
     
+    # this now exists
     def __init__(self, lyrics):
         self.lyrics = lyrics
     
+    # this is a Song function to print lyrics
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
 
+# new song variables using class, passing lists of strings as lyrics
 happy_bday = Song(["Happy birthday to you",
                     "I don't want to get sued",
                     "So I'll stop right there"])
@@ -14,6 +18,7 @@ happy_bday = Song(["Happy birthday to you",
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
 
+# call song function for each of our song variables
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
